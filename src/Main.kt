ed9b7d7bef328cb08a867a90e5a91c2643cd89b3
bibou12 <kheddali.chakib@gmail.com>
2024-06-25 19:10:28 +0200

    "first manipulation"

---
+++ src/Main.kt
@@ -0,0 +1,17 @@
+import java.util.Scanner
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+
+    var number1:Int=5
+    var number2:Int=10
+    var name="chakib"
+    println(name.startsWith("ch"))
+    println(name.first())
+    println(name.last())
+    println( "number1+number2=${number1+number2}")
+
+
+
+}
