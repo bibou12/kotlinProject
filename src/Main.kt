268c4b8185e3434a3b977500faf1bb29f799b803
bibou12 <kheddali.chakib@gmail.com>
2024-06-26 06:33:39 +0200

    "nested loop"

--- src/Main.kt
+++ src/Main.kt
@@ -3,15 +3,26 @@ import java.util.Scanner
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
+for (i in 1..5){
+    for (z in 5 downTo i){
+        print(" ")
+    }
+    for (j in 1..i){
+        print("*")
+    }
+    var number=i-1
+    for (c in 1..number){
+        print("*")
+    }
+    println()
 
-    var number1:Int=5
-    var number2:Int=10
-    var name="chakib"
-    println(name.startsWith("ch"))
-    println(name.first())
-    println(name.last())
-    println( "number1+number2=${number1+number2}")
+}
 
 
 
 }
+
+
+
+
+
